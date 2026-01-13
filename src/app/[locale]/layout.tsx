ad4526f324835c39cccf104b72f0455cd7ed08b8
Barbaros Ahmet Bayram <barbaros@ardasaglobal.com>
2026-01-13 22:51:46 +0000

    lets do step 3 but lets do it very carefully, checking and re checking e

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next';
-import './globals.css';
+import '../globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
@@ -9,11 +9,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params: {locale}
 }: Readonly<{
   children: React.ReactNode;
+  params: {locale: string};
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang={locale} className="scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
