429953f1943d8f6efb0a6c545885ec140a28b8b2
Barbaros Ahmet Bayram <barbaros@ardasaglobal.com>
2026-01-24 02:35:27 +0000

    Goal: Fix Firebase App Hosting 404/400 for local static assets under /pu

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
