a140b204a08de9e4a58ab3e87ac8a9e590d8997e
Barbaros Ahmet Bayram <barbaros@ardasaglobal.com>
2026-01-25 18:24:26 +0000

    G-T9VLGTQEQE

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import '../globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import {getMessages} from 'next-intl/server';
@@ -36,6 +37,22 @@ export default function RootLayout({
           {children}
           <Toaster />
         </NextIntlClientProvider>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `,
+          }}
+        />
       </body>
     </html>
   );
